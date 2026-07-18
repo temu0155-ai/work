a5c48137a6adbd1ef9ea516a3a87c42d88c7cc32
temu0155-ai <temu0155@gmail.com>
2026-07-18 14:11:58 +0200

    Add message XP handling on message creation

---
+++ events/messageCreate.js
@@ -0,0 +1,21 @@
+const { Events } = require('discord.js');
+const { addMessageXp } = require('../utils/xp');
+
+module.exports = {
+  name: Events.MessageCreate,
+  once: false,
+  async execute(message) {
+    if (message.author.bot || !message.guild) return;
+
+    try {
+      const result = await addMessageXp(message.guild.id, message.author.id);
+      if (result?.leveledUp) {
+        message.channel
+          .send(`🎉 ${message.author} just reached **level ${result.level}**!`)
+          .catch(() => {}); // don't crash if the bot lacks send perms in this channel
+      }
+    } catch (err) {
+      console.error('[xp] Failed to award message XP:', err);
+    }
+  },
+};
